54da02d62059bbfee2d695374fd4d13d4b6bd1c4
whals <whalsdud0131@gmail.cm>
2025-06-18 10:03:54 +0900

    sort

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,22 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Iterator;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
+        List<String> fruits = new ArrayList<>();
 
+        fruits.add("Mango");
+        fruits.add("Apple");
+        fruits.add("Cherry");
+        fruits.add("Banana");
+        fruits.add("Dragonfruit");
+
+        Collections.sort(fruits);
+
+        for(String fruit: fruits) {
+            System.out.println(fruit);
+        }
     }
 }
